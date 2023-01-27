7bf7485a97e905c4d44fa858ad5d0d1c18133067
Leandro Arruda <leandrolevvi@hotmail.com>
2023-01-26 22:13:07 -0300

    Adicionado models

---
+++ Main/Models/User.cs
@@ -0,0 +1,13 @@
+﻿namespace Main.Models
+{
+    public class User
+    {
+        public int Id { get; set; } = Int32.MinValue;
+        public string Slug { get; set; } = String.Empty;
+        public string Name { get; set; } = String.Empty;
+        public string Email { get; set; } = String.Empty;
+        public string PasswordHash { get; set; } = String.Empty;
+        public string linkProfileImage { get; set; } = String.Empty;
+        public List<Role> Roles { get; set; } = new List<Role>();
+    }
+}
