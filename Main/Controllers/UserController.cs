a0fb45766ab5bc6da738d9959d8cbf00fd56b0fd
Leandro Arruda <leandrolevvi@hotmail.com>
2023-01-28 16:16:19 -0300

    Adicionado Get and GetById para o User

--- Main/Controllers/UserController.cs
+++ Main/Controllers/UserController.cs
@@ -7,6 +7,7 @@ using Main.ViewModel;
 using Main.ViewModel.EditorViewModel;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.IdentityModel.Tokens;
 using SecureIdentity.Password;
@@ -17,7 +18,8 @@ namespace Main.Controllers
     public class UserController : ControllerBase
     {
         [HttpPost("v1/users")]
-        public async Task<ActionResult> CreateAsync([FromServices] DbContextAccount context, [FromBody] EditorUserViewModel model)
+        public async Task<ActionResult> CreateAsync([FromServices] DbContextAccount context,
+            [FromBody] EditorUserViewModel model)
         {
             if (!ModelState.IsValid)
             {
@@ -46,6 +48,54 @@ namespace Main.Controllers
                 await context.SaveChangesAsync();
 
                 return Created($"/{user.Slug}", new ResponseViewModel<User>(user));
+            }
+            catch (DbException)
+            {
+                return BadRequest(new ResponseViewModel<string>("DB-01 - Ocorreu um erro no banco de dados"));
+            }
+            catch (Exception)
+            {
+                return BadRequest(new ResponseViewModel<string>("EG-01 - Ocorreu um erro no servidor"));
+            }
+        }
+
+        [HttpGet("/v1/users")]
+        public async Task<ActionResult> GetAsync([FromServices] DbContextAccount context,
+            [FromQuery] int pageNumber = 0, [FromQuery] int pageSize = 25)
+        {
+            try
+            {
+                var users = await context
+                    .Users
+                    .AsNoTracking()
+                    .OrderBy(x => x.Id)
+                    .Skip(pageNumber * pageSize)
+                    .Take(pageSize)
+                    .ToListAsync();
+
+                return Ok(new ResponseViewModel<List<User>>(users));
+            }
+            catch (DbException)
+            {
+                return BadRequest(new ResponseViewModel<string>("DB-01 - Ocorreu um erro no banco de dados"));
+            }
+            catch (Exception)
+            {
+                return BadRequest(new ResponseViewModel<string>("EG-01 - Ocorreu um erro no servidor"));
+            }
+        }
+
+        [HttpGet("/v1/users/{id}")]
+        public async Task<ActionResult> GetByIdAsync([FromServices] DbContextAccount context, [FromRoute] int id)
+        {
+            try
+            {
+                var user = await context
+                    .Users
+                    .AsNoTracking()
+                    .FirstOrDefaultAsync(x => x.Id == id);
+
+                return Ok(new ResponseViewModel<User>(user));
             }
             catch (DbException)
             {
