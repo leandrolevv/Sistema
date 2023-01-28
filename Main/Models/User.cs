b82c441e3f22d8c6a7b0abd33ae915feeff478ae
Leandro Arruda <leandrolevvi@hotmail.com>
2023-01-28 17:09:33 -0300

    Garantir que senha não retornará nas consultas (mesmo hasheada)

--- Main/Models/User.cs
+++ Main/Models/User.cs
@@ -1,4 +1,6 @@
-﻿namespace Main.Models
+﻿using System.Text.Json.Serialization;
+
+namespace Main.Models
 {
     public class User
     {
@@ -6,6 +8,7 @@
         public string Slug { get; set; } = String.Empty;
         public string Name { get; set; } = String.Empty;
         public string Email { get; set; } = String.Empty;
+        [JsonIgnore]
         public string PasswordHash { get; set; } = String.Empty;
         public string linkProfileImage { get; set; } = String.Empty;
         public IList<Role> Roles { get; set; } = new List<Role>();
