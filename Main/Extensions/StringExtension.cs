a1151e51a65681ec2c3e680c1f9773a840bd4135
Leandro Arruda <leandrolevvi@hotmail.com>
2023-01-30 19:20:39 -0300

    Melhoria na criação dos slugs

--- Main/Extensions/StringExtension.cs
+++ Main/Extensions/StringExtension.cs
@@ -1,10 +1,39 @@
-﻿namespace Main.Extension
+﻿using System.Globalization;
+using System.Text.RegularExpressions;
+using System.Text;
+
+namespace Main.Extension
 {
     public static class StringExtension
     {
         public static string CreateSlug(this string text)
         {
-            return text.ToLower().Replace(" ", "_").Replace("@", "_").Replace(".", "_");
+            return text.ToLower().ReplaceSeparators().ReplaceSpecialCharacters();
+        }
+
+        //Curiosidade: Os blocos abaixo foram criados com ChatGPT
+        private static string ReplaceSpecialCharacters(this string text)
+        {
+            string normalizedString = text.Normalize(NormalizationForm.FormD);
+            StringBuilder stringBuilder = new StringBuilder();
+
+            foreach (char c in normalizedString)
+            {
+                UnicodeCategory unicodeCategory = CharUnicodeInfo.GetUnicodeCategory(c);
+                if (unicodeCategory != UnicodeCategory.NonSpacingMark)
+                {
+                    stringBuilder.Append(c);
+                }
+            }
+
+            return Regex.Replace(stringBuilder.ToString(), @"[^\w\s]", "").Trim();
+        }
+
+        private static string ReplaceSeparators(this string text)
+        {
+            string pattern = @"[@.\s]+";
+            string replacement = "_";
+            return Regex.Replace(text, pattern, replacement);
         }
     }
 }
